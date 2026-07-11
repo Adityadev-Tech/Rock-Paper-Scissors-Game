ac30ee9f99b31c4be5985d8e18c8e8b3c87e29b7
Adityadev-Tech <innovativehustler01@gmail.com>
2026-07-11 19:39:28 +0530

    Updated the javaScript logic

--- scrip.js
+++ scrip.js
@@ -1,4 +1,69 @@
 
-// options user has
-let UserSelect = document.querySelectorAll("#images-container img");
-console.log(UserSelect.id);
+let UserScore = 0 , CompScore = 0 ;
+// getting all the elements 
+let GivenChoices = document.querySelectorAll("#images-container img");
+let showfinalMsg = document.querySelector("#finalmsg") ;
+let Uscore = document.querySelector("#Unum") ;
+let Cscore = document.querySelector("#Cnum") ;
+
+
+
+
+
+// generating a random computer choice
+const ComputerChoice = () => {
+    let AvailOptions = ["rock", "paper", "scissors"];
+    let idx = Math.floor(Math.random() * 3);
+    return AvailOptions[idx];
+} 
+//updation of scores 
+const showingTheWinner = (WhatCOMPchoose ,UserChoice ,DidUserWin)=> {
+    if(DidUserWin) {
+        UserScore++ ;
+        Uscore.innerText = UserScore;
+        showfinalMsg.innerText = "Congratulation You Won!" ;
+        showfinalMsg.style.background = "#27F56F" ; 
+        
+        
+        
+    }
+    else {
+        CompScore++ ;
+        Cscore.innerText =CompScore ;
+        showfinalMsg.innerText ="You lost ! Try Again" ;
+        showfinalMsg.style.background = "#F52727" ; 
+    }
+}
+
+// starting the game 
+const gamePlay = (UserChoice) => {
+    const WhatCOMPchoose = ComputerChoice();
+
+    if (WhatCOMPchoose === UserChoice) {
+        showfinalMsg.innerText= "The match is a draw" ; 
+        showfinalMsg.style.background = "#E727F5";
+
+    }
+    else {
+        let DidUserWin;
+        if (UserChoice === "rock") {
+            DidUserWin = WhatCOMPchoose === "scissors" ? true : false;
+        }
+        else if (UserChoice === "paper") {
+            DidUserWin = WhatCOMPchoose === "rock" ? true : false;
+        }
+        else {
+            DidUserWin = WhatCOMPchoose === "paper" ?true:false;
+        }
+        showingTheWinner(WhatCOMPchoose ,UserChoice ,DidUserWin) ; 
+    }
+
+}
+
+// Getting the choice of the user
+GivenChoices.forEach((options) => {
+    options.addEventListener("click", () => {
+        const UserChoice = options.id;
+        gamePlay(UserChoice) ;
+    });
+});
